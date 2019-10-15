91870159dde6eea82d1ba15b82b1c4c513b32ce3
staneee <yi.hang@live.com>
2019-10-15 12:11:30 +0800

    增加hmr模式

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {environment} from './environments/environment';
-import {RootModule} from './root.module';
+import { environment } from './environments/environment';
+import { RootModule } from './root.module';
+
+import { hmrBootstrap } from './hmr';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(RootModule)
-  .catch(err => console.error(err));
+const bootstrap = () => {
+  return platformBrowserDynamic().bootstrapModule(RootModule);
+};
+
+if (environment.hmr && module['hot']) {
+  console.log('hmr mode');
+  hmrBootstrap(module, bootstrap);
+} else {
+  console.log('default mode');
+  bootstrap().catch(err => console.error(err));
+}
